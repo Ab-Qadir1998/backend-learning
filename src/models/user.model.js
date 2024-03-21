3e3c841bab5f25a8d77080983165b6ae4716f0d3
Ab-Qadir1998 <abdulqadir12356@gmail.com>
2024-03-22 01:37:02 +0500

    working on userModel (Password and JWT)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import { Jwt } from "jsonwebtoken";
 const UserSchema = new Schema(
   {
     username: {
@@ -46,5 +47,41 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  return (
+    jwt.sign({
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    }),
+    process.env.ACCESS_TOKEN,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+UserSchema.methods.generateRefreshToken = function () {
+  return (
+    jwt.sign({
+      _id: this._id,
+    }),
+    process.env.REFRESH_TOKEN,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", UserSchema);
